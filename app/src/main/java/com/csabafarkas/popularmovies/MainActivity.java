303070e4ebe3e74d79ec4f9f4e9f9822eaf26a70
lastminute84 <csabafarkas17@gmail.com>
2018-03-09 14:14:11 +0000

    Transforming to a git-flow repo

--- app/src/main/java/com/csabafarkas/popularmovies/MainActivity.java
+++ app/src/main/java/com/csabafarkas/popularmovies/MainActivity.java
@@ -2,12 +2,25 @@ package com.csabafarkas.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.iv) ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Picasso.with(this)
+                .load("http://i.imgur.com/DvpvklR.png")
+                .placeholder(R.drawable.ic_icon_img_placeholder)
+                .into(imageView);
     }
 }
---
+++ app/src/main/java/com/csabafarkas/popularmovies/models/Movie.java
@@ -0,0 +1,9 @@
+package com.csabafarkas.popularmovies.models;
+
+/**
+ * Created by csabafarkas on 09/03/2018.
+ */
+
+public class Movie {
+    
+}
